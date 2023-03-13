2c5dfdbe188eac07fe9e5ae3a7bb650728c11f02
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-13 22:26:53 +0100

    add starter files and change to TS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Theme from '../styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Theme>
+        <Component {...pageProps} />
+      </Theme>
+    </>
+  );
 }
+ 
