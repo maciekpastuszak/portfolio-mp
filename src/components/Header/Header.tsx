c1477d2707cf06fdd65e50e8d4215061a6a7eb5f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-14 09:44:10 +0100

    add Span style component to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/" legacyBehavior>
         <a href="/" style={{ display: 'flex', alignItems: 'center', color: 'white'}}>
-        <DiCssdeck size='3rem' /><span>Portfolio</span>
+        <DiCssdeck size='3rem' /><Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
