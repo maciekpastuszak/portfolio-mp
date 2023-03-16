b61f39f20ea6a822f7d09b87f9e32c79d74d9787
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-16 11:02:01 +0100

    add achievements section with mapping boxes

--- src/components/Acomplishments/Acomplishments.tsx
+++ src/components/Acomplishments/Acomplishments.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { data } from '../../constants/constants';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Personal Acheivements</SectionTitle>
+    <Boxes>
+        {data.map((card, index) => (
+          <Box key={index}>
+            
+          </Box>
+        ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
